a34fce7e41f9b870fe26ee6f7c0fdf909069253a
yutongyaF2023 <yutongya@andrew.cmu.edu>
2023-11-12 19:54:46 -0800

    historical price calc update

--- HistoricalSpreadCalc.fs
+++ HistoricalSpreadCalc.fs
@@ -37,41 +37,53 @@ let loadHistoricalValuesFile () : list<Quote> =
     // Format: (CurrencyPair, Exchange, BidPrice, AskPrice)
     []
 
+let bucketizeQuote (quote: Quote) : int =
+    // Convert timestamp to bucket number, assuming quote.Time is in milliseconds
+    quote.Time / 5
+
 /// Regroups quotes into buckets of 5 milliseconds
-let regroupQuotesIntoBuckets (quotes: list<Quote>) : list<BucketedQuotes> = 
-    // bucketing logic
-    // Returns: List of buckets with (CurrencyPair, List of (BidPrice, AskPrice))
-    []
+let regroupQuotesIntoBuckets (quotes: list<Quote>) : list<BucketedQuotes> =
+    quotes
+    |> List.groupBy bucketizeQuote
+    |> List.map (fun (bucketKey, bucketQuotes) -> { Quotes = bucketQuotes })
 
-/// Identifies arbitrage opportunity in a given bucket
-let identifyArbitrageOpportunity (bucket: BucketedQuote) : ArbitrageOpportunity option =
-    // arbitrage identification logic
-    // Identifies opportunities where price difference is more than $0.01
-    None
+/// Selects the quote with the highest bid price for each exchange
+let selectHighestBidPerExchange (quotes: list<Quote>) : list<Quote> =
+    quotes
+    |> List.groupBy (fun quote -> quote.Exchange)
+    |> List.collect (fun (_, quotesByExchange) -> 
+        quotesByExchange 
+        |> List.maxBy (fun quote -> quote.BidPrice)
+        |> List.singleton)
 
-/// Persists the identified arbitrage opportunities
-let persistArbitrageOpportunities (opportunities: ArbitrageOpportunity list) : bool =
-    // persisting logic
-    // Could save to database or file
-    true
+/// Identifies arbitrage opportunities within a list of quotes
+let identifyArbitrageOpportunities (quotes: list<Quote>) : list<ArbitrageOpportunity> =
+    let combinations = List.allPairs quotes quotes
+    combinations
+    |> List.choose (fun (quote1, quote2) ->
+        if quote1.CurrencyPair = quote2.CurrencyPair && quote1.Exchange <> quote2.Exchange then
+            let priceDifference = abs (quote1.BidPrice - quote2.AskPrice)
+            if priceDifference > 0.01m then
+                Some { Currency1 = fst quote1.CurrencyPair; Currency2 = snd quote1.CurrencyPair; NumberOfOpportunitiesIdentified = 1 }
+            else None
+        else None)
 
 // -------------------------
 // Workflow Implementation
 // -------------------------
 
-/// Workflow to calculate historical spread and identify arbitrage opportunities
 let calculateHistoricalSpreadWorkflow (request: HistoricalSpreadCalculationRequested) : ArbitrageOpportunitiesIdentified option =
     let historicalValues = loadHistoricalValuesFile ()
     let buckets = regroupQuotesIntoBuckets historicalValues
     let opportunities = 
         buckets 
-        |> List.map identifyArbitrageOpportunity 
-        |> List.choose id  
+        |> List.collect (fun bucket -> 
+            let selectedQuotes = selectHighestBidPerExchange bucket.Quotes
+            identifyArbitrageOpportunities selectedQuotes)
+        |> List.groupBy (fun op -> (op.Currency1, op.Currency2))
+        |> List.map (fun ((currency1, currency2), ops) -> 
+            { Currency1 = currency1; Currency2 = currency2; NumberOfOpportunitiesIdentified = List.sumBy (fun op -> op.NumberOfOpportunitiesIdentified) ops })
 
     match opportunities with
     | [] -> None  // No opportunities identified
-    | _ ->
-        if persistArbitrageOpportunities opportunities then
-            Some opportunities  // Successfully identified and persisted opportunities
-        else
-            None  // Failed to persist opportunities
+    | _ -> Some opportunities  // Successfully identified opportunities
