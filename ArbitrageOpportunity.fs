c230f667d378b1c0f52b9554cc682e80184528d2
yutongz7S2023 <yutongz7@andrew.cmu.edu>
2023-11-28 18:16:55 -0800

    Added support for Polygon

--- ArbitrageOpportunity.fs
+++ ArbitrageOpportunity.fs
@@ -1,7 +1,13 @@
 module ArbitrageOpportunity
 
+open Suave.Sockets
+open Suave.Sockets.Control
+open Suave.WebSocket
 open OrderManagement
 open TradingStrategy
+
+let apiKey = "qC2Ix1WnmcpTMRP2TqQ8hVZsxihJq7Hq";
+let private httpClient = new HttpClient()
 
 // ---------------------------
 // Types and Event Definitions
@@ -17,6 +23,26 @@ type Quote = {
     AskSize: Quantity;
     Time: Time;
 }
+type UnprocessedQuote = {
+    [<JsonProperty("ev")>]
+    EventType: string
+    [<JsonProperty("pair")>]
+    CurrencyPair: string
+    [<JsonProperty("bp")>]
+    BidPrice: Quantity
+    [<JsonProperty("bs")>]
+    BidSize: Quantity
+    [<JsonProperty("ap")>]
+    AskPrice: Price
+    [<JsonProperty("as")>]
+    AskSize: Price
+    [<JsonProperty("t")>]
+    Time: Time
+    [<JsonProperty("x")>]
+    Exchange: int
+    [<JsonProperty("r")>]
+    ReceiveTime: Time
+} 
 
 type Event = 
     | QuoteFeedSubscribed
@@ -31,7 +57,7 @@ type RealTimeDataFeedUnubscribed = {
     CurrencyPairs: CurrencyPair list
 }
 type MarketDataRetrieved = {
-    Quote: Quote
+    Quotes: Quote list
 }
 type OrderEmitted = {
     Orders: OrderDetails list
@@ -99,34 +125,111 @@ let krakenAgent =
 // ----------
 
 // Workflow: After trading strategy is activated, subscribe to real-time data feed
-let subscribeToRealTimeDataFeed (input: TradingStrategyActivated) = 
-    true // placeholder for subscribing logic
 
+// Helper that fetches crypto pairs
+let fetchCryptoPairsFromDB = 
+// TODO: db operation
+   |> List.fold (fun acc pair -> acc + ",XQ." + pair) ""
+
+// Subscribe to real time data via Polygon
+let subscribeToRealTimeDataFeed (input: TradingStrategyActivated) = 
+    async {
+        // connect to Polygon and authenticate
+        let url = "wss://socket.polygon.io/crypto"
+        let authPayload = {
+            action = "auth",
+            params = apiKey,
+        }
+        let authJson = JsonConvert.SerializeObject(authPayload)
+        let authContent = new StringContent(authJson, Encoding.UTF8, "application/json")
+        do! httpClient.PostAsync(url, authContent) |> Async.AwaitTask
+
+        // connect to websocket
+        let ws = new ClientWebSocket()
+        do! ws.ConnectAsync(url, CancellationToken.None) |> Async.AwaitTask
+
+        // subscribe
+        let! cryptoPairs = fetchCryptoPairsFromDB
+        let subscribePayload = {action = "subscribe", params = cryptoPairs};
+        let subscribeJson = JsonConvert.SerializeObject(authPayload)
+        let subscribeContent = new StringContent(authJson, Encoding.UTF8, "application/json")
+        do! httpClient.PostAsync(url, subscribeContent) |> Async.AwaitTask
+    }
+
+// Workflow: Pause trading when trading strategy is deactivated
+let unsubscribeRealTimeDataFeed (ws: ClientWebSocket) = 
+    async {
+        do! ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Trading Stopped", CancellationToken.None) 
+            |> Async.AwaitTask
+    }
 
 // Workflow: Retrieve data from real-time data feed
-let retrieveDataFromRealTimeFeed (input: QuoteFeedSubscribed) : MarketDataRetrieved = 
-    true // placeholder for fetching data from feed and updating data at agent
-    // The returned event MarketDataRetrieved is for one single Quote, 
-    // this is under the assumption that the real time market data keeps coming
-    // in separately (one quote for one currency pair each time). Each time a 
-    // quote comes in, we do the trading immediately
 
+// Helper that parses JSON array received from Polygon to a quote list
+let parseJsonArray (res: string) : Quote list =
+    let unprocessedQuotes = JsonConvert.DeserializeObject<UnprocessedQuote[]>(res) |> Array.toList
+    List.map (fun quote -> {
+        Exchange = getExchangeFromQuote quote
+        CurrencyPair = {
+            Currency1 = quote.CurrencyPair.[0..2]
+            Currency2 = quote.CurrencyPair.[3..5]
+        };
+        BidPrice = quote.BidPrice;
+        AskPrice = quote.AskPrice;                          
+        BidSize = quote.BidSize;
+        AskSize = quote.AskSize;
+        Time = quote.Time;
+    }) unprocessedQuotes
+
+// Helper that continuously receives market data from websocket and assess 
+// arbitrage opportunities when trading strategy is activated, 
+let rec receiveMsgFromWSAndTrade (ws: ClientWebSocket) = 
+    async {
+        let tradingStrategyActivated = tradingStrategyAgent.PostAndReply(RetrieveTradingStrategyStatus)
+        match tradingStrategyActivated with
+        | true ->
+            let buffer = ArraySegment<byte>(Array.zeroCreate 2048)
+            let! response = ws.ReceiveAsync(buffer, CancellationToken.None) |> Async.AwaitTask
+            let message = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, response.Count)
+            let quotes = parseJsonArray message
+            // assess arbitrage opportunity for all quotes 
+            let retrievedMarketData = {Quotes = quotes}
+            assessRealTimeArbitrageOpportunity retrievedMarketData
+            return! receiveMsgFromWS ws
+        | false -> unsubscribeRealTimeDataFeed ws
+    }
+
+// Continuously receives market data from websocket and trade
+let retrieveDataFromRealTimeFeedAndTrade (input: QuoteFeedSubscribed) : MarketDataRetrieved = 
+    async {
+        do! receiveMsgFromWSAndTrade ws |> Async.AwaitTask
+    }
 
 // Workflow: Assess Real Time Arbitrage Opportunity
+
 let strategy = tradingStrategyAgent.PostAndReply(GetParams)
 let currentDailyVolume = volumeAgent.PostAndReply(CheckCurrentVolume)
 
+// Helper that checks whether there is valid price spread
 let minPriceSpreadReached (ask: Quote) (bid: Quote) = 
     let priceSpread = bid.BidPrice - ask.AskPrice
     priceSpread >= strategy.minPriceSpread
 
-let getAgentFromQuote (data: Quote) = 
+// Helper that returns the correct agent based on quote
+let getAgentFromUnprocessedQuote (data: Quote) = 
     match data.Exchange with
-    | Bitstamp -> bitstampAgent
-    | Bitfinex -> bitfinexAgent
-    | Kraken -> krakenAgent
+    | 2 -> bitfinexAgent
+    | 6 -> bitstampAgent
+    | 23 -> krakenAgent
 
-// calculate order volume based on user provided limits
+// Helper that returns the correct exchange based on quote
+let getExchangeFromUnprocessedQuote (data: Quote) = 
+    match data.Exchange with
+    | 2 -> "Bitfinex"
+    | 6 -> "Bitstamp"
+    | 23 -> "Kraken"
+
+// Helper that calculates order volume based on user provided limits
 let calculateWorthwhileTransactionVolume (ask: Quote) (bid: Quote) = 
     let idealVolume = min ask.AskSize bid.BidSize
     let minProfitReached = idealVolume * priceSpread >= strategy.MinTransactionProfit
@@ -138,6 +241,7 @@ let calculateWorthwhileTransactionVolume (ask: Quote) (bid: Quote) =
         min maxVolumeUnderTotalAmountLimit maxVolumeUnderDailyVolumeLimit
     | false -> 0
 
+// Helper that identifies worthwhile orders for an ask-bid pair
 let identifyWorthwhileTransactions (ask: Quote) (bid: Quote) = 
     match minPriceSpreadReached ask bid with
     | true ->   
@@ -160,6 +264,7 @@ let identifyWorthwhileTransactions (ask: Quote) (bid: Quote) =
 
             let bidAgent = getAgentFromQuote bid
             let remainingBidData = {
+                EventType = ask.EventType
                 Exchange = bid.Exchange;
                 CurrencyPair = bid.CurrencyPair;
                 AskPrice = bid.AskPrice;
@@ -167,6 +272,7 @@ let identifyWorthwhileTransactions (ask: Quote) (bid: Quote) =
                 BidPrice = bid.BidPrice;
                 BidSize = bid.BidSize - worthwhileTransactionVolume;
                 Time = bid.Time;
+                ReceiveTime = bid.ReceiveTime
             }
             bidAgent.Post(UpdateData remainingBidData)
 
@@ -189,24 +295,30 @@ let identifyWorthwhileTransactions (ask: Quote) (bid: Quote) =
     | false -> []
 
 // Filter all quotes with same currency pair from other exchanges to identify 
-// arbitrage opportunity and emit corresponding orders
+// arbitrage opportunity and emit corresponding orders for each quote
 let assessRealTimeArbitrageOpportunity (marketDataRetrieved: MarketDataRetrieved): OrderEmitted = 
-    let quote = marketDataRetrieved.Quote
-    let bitstampData = bitstampAgent.PostAndReply(RetrieveLatestData) 
-    let bitfinexData = bitfinexAgent.PostAndReply(RetrieveLatestData)
-    let krakenData = krakenAgent.PostAndReply(RetrieveLatestData)
-    let allQuotes = bitstampData @ bitfinexData @ krakenData
-    let quotes = allQuotes 
-        |> List.filter(fun x -> quote.Exchange <> x.Exchange && quote.CurrencyPair = x.CurrencyPair) 
-        |> List.fold (fun acc otherQuote ->
-            acc @ identifyWorthwhileTransactions quote otherQuote @ identifyWorthwhileTransactions otherQuote quote
-        ) []
+    let quotes = marketDataRetrieved.Quotes
+    quotes |> List.fold (fun acc quote ->
+        let bitstampData = bitstampAgent.PostAndReply(RetrieveLatestData) 
+        let bitfinexData = bitfinexAgent.PostAndReply(RetrieveLatestData)
+        let krakenData = krakenAgent.PostAndReply(RetrieveLatestData)
+        let allQuotes = bitstampData @ bitfinexData @ krakenData
+        let orders = allQuotes 
+            |> List.filter(fun x -> quote.Exchange <> x.Exchange && quote.CurrencyPair = x.CurrencyPair) 
+            |> List.fold (fun acc otherQuote ->
+                acc @ identifyWorthwhileTransactions quote otherQuote @ identifyWorthwhileTransactions otherQuote quote
+            ) []
+        acc @ orders
+    )[]
 
 
-// Workflow: Pause trading when trading strategy is deactivated
-let unsubscribeRealTimeDataFeed (input: TradingStrategyDeactivated) = 
-    true // Placeholder for unsubscribing logic
-
-
+// Following is a sample code that runs the trading algo, doesn't belong to any workflow
+let sampleRun = 
+    async {
+        do! subscribeToRealTimeDataFeed |> Async.AwaitTask
+        do! retrieveDataFromRealTimeFeedAndTrade |> Async.AwaitTask
+    }
+let main = 
+    sampleRun |> Async.RunSynchronously
 
 
