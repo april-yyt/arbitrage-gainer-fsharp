7583a7a830ade8249f60a35aa4c7b6400a2cf4b8
april-yyt <aprilytyang@gmail.com>
2023-12-04 09:35:38 -0800

    mock api

--- api/BitfinexAPI.fs
+++ api/BitfinexAPI.fs
@@ -8,8 +8,9 @@ let private httpClient = new HttpClient()
 
 let submitOrder (orderType: string) (symbol: string) (amount: string) (price: string) =
     async {
-        let url = "https://api.bitfinex.com/v2/auth/w/order/submit"
-        let payload = sprintf "{\"type\": \"%s\", \"symbol\": \"%s\", \"amount\": \"%s\", \"price\": \"%s\"}" orderType symbol amount price
+        let url = "https://18656-testing-server.azurewebsites.net/order/place/v2/auth/w/order/submit" // Updated URL
+        // Adjust the body to match the mock API format
+        let payload = sprintf "{\"type\": \"MARKET\", \"symbol\": \"%s\", \"amount\": \"%s\", \"price\": \"%s\"}" "t" + symbol amount price
         let content = new StringContent(payload, Encoding.UTF8, "application/json")
         let response = await httpClient.PostAsync(url, content)
         match response.IsSuccessStatusCode with
@@ -22,8 +23,8 @@ let submitOrder (orderType: string) (symbol: string) (amount: string) (price: st
 
 let retrieveOrderTrades (symbol: string) (orderId: int) =
     async {
-        let url = sprintf "https://api.bitfinex.com/v2/auth/r/order/%s:%d/trades" symbol orderId
-        let response = await httpClient.PostAsync(url, null)
+        let url = sprintf "https://18656-testing-server.azurewebsites.net/order/status/auth/r/order/%s:%d/trades" "t" + symbol orderId // Updated URL
+        let response = await httpClient.GetAsync(url) // Changed from PostAsync to GetAsync
         match response.IsSuccessStatusCode with
         | true -> 
             let! responseString = response.Content.ReadAsStringAsync()
