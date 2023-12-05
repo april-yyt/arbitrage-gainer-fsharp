00dc432d35ef23956e47e830bb40020226c3509d
yutongyaF2023 <yutongya@andrew.cmu.edu>
2023-12-04 21:23:52 -0800

    BITSTAMP BUG FIX

--- api/BitstampAPI.fs
+++ api/BitstampAPI.fs
@@ -1,39 +1,47 @@
 module BitstampAPI
 
+open System
 open System.Net.Http
 open System.Text
 open Newtonsoft.Json
-open System
+open System.Diagnostics
 
-let private httpClient = new HttpClient(BaseAddress = Uri("https://18656-testing-server.azurewebsites.net")) // Updated Base URL
+let private httpClient = new HttpClient(BaseAddress = Uri("https://18656-testing-server.azurewebsites.net"))
 
 let postRequest (url: string) (data: (string * string) list) =
     async {
-        let content = new FormUrlEncodedContent(data)
-        let response = await httpClient.PostAsync(url, content)
+        let content = new FormUrlEncodedContent(data |> Seq.map (fun (k, v) -> new System.Collections.Generic.KeyValuePair<string, string>(k, v)))
+
+        let! response = Async.AwaitTask (httpClient.PostAsync(url, content)) 
         match response.IsSuccessStatusCode with
         | true -> 
-            let! responseString = response.Content.ReadAsStringAsync()
-            return Some(responseString) // Parse JSON response 
+            let! responseString = Async.AwaitTask (response.Content.ReadAsStringAsync()) 
+            Debug.WriteLine($"Response JSON: {responseString}")
+            return Some(responseString)
         | false -> 
+            Debug.WriteLine($"Error: {response.StatusCode}")
             return None
     }
 
+
 let buyMarketOrder (marketSymbol: string) (amount: string) (clientOrderId: Option<string>) =
-    let url = sprintf "/order/place/api/v2/buy/market/%s/" marketSymbol.ToLower() // Updated URL
+    let url = sprintf "/order/place/api/v2/buy/market/%s/" (marketSymbol.ToLower())
     let data = 
-        [ "amount", amount; "price", "dummyValue" ] // Adjusted data format
+        [ "amount", amount; "price", "dummyValue" ]
         |> List.append (clientOrderId |> Option.map (fun id -> [ "client_order_id", id ]) |> Option.defaultValue [])
+    Debug.WriteLine($"Sending buyMarketOrder to URL: {url} with data: {data}")
     postRequest url data
 
 let sellMarketOrder (marketSymbol: string) (amount: string) (clientOrderId: Option<string>) =
-    let url = sprintf "/order/place/api/v2/sell/market/%s/" marketSymbol.ToLower() // Updated URL
+    let url = sprintf "/order/place/api/v2/sell/market/%s/" (marketSymbol.ToLower())
     let data = 
-        [ "amount", amount; "price", "dummyValue" ] // Adjusted data format
+        [ "amount", amount; "price", "dummyValue" ]
         |> List.append (clientOrderId |> Option.map (fun id -> [ "client_order_id", id ]) |> Option.defaultValue [])
+    Debug.WriteLine($"Sending sellMarketOrder to URL: {url} with data: {data}")
     postRequest url data
 
 let orderStatus (orderId: string) =
-    let url = "/order/status/api/v2/order_status/" // Updated URL
+    let url = "/order/status/api/v2/order_status/"
     let data = [ "id", orderId ]
+    Debug.WriteLine($"Checking orderStatus for orderId: {orderId}")
     postRequest url data
