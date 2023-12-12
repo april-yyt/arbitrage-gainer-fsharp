2b631a78be6ac0e28d6ef5a8e073e25c31155b42
April Yang <yutongya@andrew.cmu.edu>
2023-12-12 13:12:28 -0800

    email notification

---
+++ Email.fs
@@ -0,0 +1,39 @@
+open System.Threading
+open SendGrid
+open SendGrid.Helpers.Mail
+open ServiceBus
+
+let sendEmailAsync (emailContent: string) =
+    async {
+        let client = SendGridClient("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")
+        let from = EmailAddress("yutongya@andrew.cmu.edu", "Example User")
+        let subject = "New Message from Service Bus"
+        let to = EmailAddress("aprilytyang@gmail.com", "Recipient User")
+        let plainTextContent = emailContent
+        let htmlContent = $"<strong>{emailContent}</strong>"
+        let msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent)
+        return! client.SendEmailAsync(msg) |> Async.AwaitTask
+    }
+
+let checkForMessagesAsync (queueName: string) =
+    async {
+        let client = ServiceBusClient(namespace, DefaultAzureCredential())
+        let receiver = client.CreateReceiver(queueName)
+        
+        while true do
+            let! receivedMessage = receiver.ReceiveMessageAsync() |> Async.AwaitTask
+            match receivedMessage with
+            | null -> () // No message, continue
+            | message ->
+                let emailContent = message.Body.ToString()
+                do! sendEmailAsync emailContent
+                do! receiver.CompleteMessageAsync(message) |> Async.AwaitTask
+
+        receiver.DisposeAsync().AsTask().Wait()
+        client.DisposeAsync().AsTask().Wait()
+    }
+
+[<EntryPoint>]
+let main argv =
+    checkForMessagesAsync "out_queue" |> Async.RunSynchronously
+    0
