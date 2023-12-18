3bde508156165d5e42fe3b089557ea4d7a6639ff
yutongyaF2023 <yutongya@andrew.cmu.edu>
2023-12-18 00:46:29 -0700

    modify endpoints

--- WebServer.fs
+++ WebServer.fs
@@ -32,7 +32,7 @@ let main args =
     }
     |> Async.Start
     async {
-        do! receiveAndProcessOrders ()
+        do! receiveAndProcessOrdersAkka ()
     }
     |> Async.Start
     let cfg = defaultConfig
